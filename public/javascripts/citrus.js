ad8156aaf1f8738e6ce3ed10b08a5e957cf93e53
unknown <siuching@385085-VAN-R.marvel.elnk.net>
2012-09-14 01:57:36 -0700

    Adjusted popup size

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,3 +1,3 @@
 function promotion_of_the_month() {
-  window.open("/images/snrmonth.jpg", "promotion", "height=578px,width=882px,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");
+  window.open("/images/snrmonth.jpg", "promotion", "height=595px,width=887px,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");
 }
