25aa68003bc42ac6545151869fe88a2d49ed3dbc
unknown <siuching@385085-VAN-R.marvel.elnk.net>
2012-11-12 18:43:35 -0800

    Updated popup

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,3 +1,3 @@
 function promotion_of_the_month() {
-  window.open("/popup", "promotion", "height=595px,width=900px,menubar=no,scrollbars=yes,status=no,toolbar=no,titlebar=no");
+  window.open("/popup", "promotion", "height=715px,width=1000px,menubar=no,scrollbars=yes,status=no,toolbar=no,titlebar=no");
 }
