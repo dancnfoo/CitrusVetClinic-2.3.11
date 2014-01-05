3d5da22617b5f2a66ddc6ab6d76cf72a5b6fb9cb
unknown <siuching@385085-VAN-R.marvel.elnk.net>
2014-01-05 01:05:16 -0800

    Dental month promotion

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,3 +1,2 @@
 function promotion_of_the_month() {
-  // window.open("/popup", "promotion", "height=715px,width=1000px,menubar=no,scrollbars=yes,status=no,toolbar=no,titlebar=no");
-}
+  window.open("/popup", "promotion", "height=840px,width=620px,location=no,menubar=no,scrollbars=no,status=,toolbar=no,titlebar=no");}
