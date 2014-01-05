a7437af636252e5df3d890016f2e77f5d4597a20
unknown <siuching@385085-VAN-R.marvel.elnk.net>
2014-01-05 01:46:42 -0800

    Fixed typo

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,2 +1,2 @@
 function promotion_of_the_month() {
-  window.open("/popup", "promotion", "height=840px,width=620px,location=no,menubar=no,scrollbars=no,status=,toolbar=no,titlebar=no");}
+  window.open("/popup", "promotion", "height=840px,width=620px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
