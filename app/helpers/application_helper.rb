27283dd50cf2f9f5a4622a4d29f361c46ca09204
unknown <siuching@608029-VAN.marvel.elnk.net>
2014-02-19 03:22:03 -0800

    Updated title to be more SEO friendly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 module ApplicationHelper
   # Return a title on a per-page basis
   def title
-    base_title = "Citrus Veterinary Clinic"
+    base_title = "Citrus Veterinary Clinic - Veterinarian in La Verne, CA 91750"
     if @title.nil?
       base_title
     else
