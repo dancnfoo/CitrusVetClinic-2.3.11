0599ed0a24864f429bbe2a6be2da0f540c9d3341
James Ching <james_s_ching@yahoo.com>
2014-09-01 09:22:35 -0700

    Senior Month Promotion

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,2 +1,2 @@
 function promotion_of_the_month() {
-  window.open("/popup", "promotion", "height=826px,width=1156px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
+  window.open("/popup", "promotion", "height=1219px,width=900px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
