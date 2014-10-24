b00f5a820a3ae269e75cb2dc81897fa0e627cbd2
James Ching <james_s_ching@yahoo.com>
2014-10-24 10:52:24 -0700

    Halloween Consume Contest 2014

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,2 +1,2 @@
 function promotion_of_the_month() {
-  window.open("/popup", "promotion", "height=1219px,width=900px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
+  window.open("/popup", "promotion", "height=720px,width=960px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
