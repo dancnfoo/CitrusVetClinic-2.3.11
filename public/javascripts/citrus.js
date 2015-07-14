bcb48e8d640aa90b7524959ef91c093cf929df0d
James Ching <james_s_ching@yahoo.com>
2015-07-13 21:21:57 -0700

    Clean up promotions

--- public/javascripts/citrus.js
+++ public/javascripts/citrus.js
@@ -1,2 +1,2 @@
 function promotion_of_the_month() {
-  window.open("/popup", "promotion", "height=720px,width=960px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
+//  window.open("/popup", "promotion", "height=602px,width=960px,location=no,menubar=no,scrollbars=no,status=no,toolbar=no,titlebar=no");}
